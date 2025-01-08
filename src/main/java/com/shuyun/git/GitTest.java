a41a0ab8ba69072661d294ae73c4f6ec5c748b29
SFengFei <17809163820@163.com>
2025-01-08 15:16:46 +0800

    msg:second commit

--- src/main/java/com/shuyun/git/GitTest.java
+++ src/main/java/com/shuyun/git/GitTest.java
@@ -5,6 +5,10 @@ public class GitTest {
     public static void main(String[] args) {
 
         System.out.println("hello git!");
+
+        System.out.println("pull stream!");
+
+        System.out.println("push!");
     }
 
 }
